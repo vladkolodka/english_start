c905f4e9a5bc732199f4bd2f661646da0a7ecb62
Vladyslav Kolodka <vladkolodka@gmail.com>
2017-11-06 22:43:05 +0200

    Fix

--- src/app/pages/VideoPage.js
+++ src/app/pages/VideoPage.js
@@ -6,39 +6,39 @@ import Divider from "material-ui/Divider";
 import AlbumIcons from "material-ui-icons/Album";
 import { withStyles } from "material-ui/styles";
 
-class VideoPage extends Component{
-    constructor(props){
+class VideoPage extends Component {
+    constructor(props) {
         super(props);
 
         this.props.loadVideo(parseInt(props.match.params.id));
     }
 
-    render(){
-        const {classes} = this.props;
-        if(!this.props.video) 
+    render() {
+        const { classes } = this.props;
+        if (!this.props.video)
             return null;
-        return(
-            <div className = {classes.container}>
+        return (
+            <div className={classes.container}>
                 <Typography type='display2' className={classes.title}>
                     <AlbumIcons />
                     {this.props.video.title}
                 </Typography>
-                
-                <Devider />
+
+                <Divider />
 
                 <object>
-                    <embed src = {this.props.video.videoUrl}/>
+                    <embed src={this.props.video.videoUrl} />
                 </object>
 
                 <Typography className={classes.title} type='body1'>
                     {this.props.video.description}
                 </Typography>
             </div>
-        );    
+        );
     }
 }
 
-const mapStateToProps = ({videos}) => ({
+const mapStateToProps = ({ videos }) => ({
     video: videos.item
 })
 
